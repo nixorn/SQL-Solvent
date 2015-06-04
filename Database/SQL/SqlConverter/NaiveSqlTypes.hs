7ad05622a313a433dbaaa495f72c0c733a3581df
nixorn <a-kanev@yandex.ru>
2015-06-04 14:20:49 +0300

    Первая версия наивных типов

--- Database/SQL/SqlConverter/NaiveSqlTypes.hs
+++ Database/SQL/SqlConverter/NaiveSqlTypes.hs
@@ -1,15 +1,21 @@
+{- Цель - сделать генерацию минимального подмножества sql, через которое можно выразить все операции-}
+
 module Database.SQL.SQLConverter.NaiveSqlTypes (
 
 	
 ) where
 import Database.SQL.SQLConverter.Types
 import Data.Text as T
+import Data.List as L
 --mssql
 
 --table, subquery
 
 
-
+data Ariphm   = Plus  | -- +
+                Minus | -- -
+                Multi | -- *
+                Div     -- /
 
 data ClauseOp = Much    | -- >
                 Less    | -- <
@@ -17,32 +23,70 @@ data ClauseOp = Much    | -- >
                 NoEqu   | -- !=
                 MEqu    | -- >=
                 LEqu      -- <=
+                
+instance Show ClauseOp where
+    show Much   = " > "
+    show Less   = " < "
+    show Equ    = " = " 
+    show NoEqu  = " != "
+    show MEqu   = " >= "
+    show LEqu   = " <= "
+
+data Logic  =   And |
+                Or    
 
 
 
 --like t1.c1 = t2.c2   
 data Clause     = Clause ((TableName, FieldName), ClauseOp, (TableName, FieldName)) 
-
-
-
+instance Show Clause where
+    show (Clause ((t1, f1), op, (t2, f2))) = show t1 ++ "." ++ show f1 ++ show op ++ show t2 ++ show f2
+    
 data Where      = Where      [Clause]
+instance Show Where where
+    show (Where clauses) = " WHERE " ++ L.foldl1 (++) (fmap show clauses)
+    
 data From       = From       TableName
-data Join       = LeftJoin | InnerJoin
-data LeftJoin   = LeftJoin   TableName [Clause]
-data InnerJoin  = InnerJoin  TableName [Clause]
+instance Show From where
+    show (From t) = " FROM " ++ T.unpack t
 
-data Select     = Select [(TableName, FieldName)] From  [Join] Where
-data InsertInto = InsertInto TableName [FieldName] Select
-data Update     = Update TableName [FieldName] 
+data Join       = LeftJoin TableName [Clause]  | InnerJoin TableName [Clause]
+instance Show Join where
+    show (LeftJoin  t clauses) = "LEFT JOIN "  ++ T.unpack t ++ " on " ++ L.foldl1 (++) (fmap show clauses) 
+    show (InnerJoin t clauses) = "INNER JOIN " ++ T.unpack t ++ " on " ++ L.foldl1 (++) (fmap show clauses) 
 
+
+
+
+data SelectList = SelectList [(TableName, FieldName)]
+instance Show SelectList where
+    show (SelectList fields) = 
+        let showL (a,b) =  T.append (T.append a $ T.append (T.pack ".") b) $ T.pack ", "
+        in  T.unpack $ L.foldl1 (T.append)   (fmap showL fields)
+    
+    
+    
+data Select = Select SelectList From  [Join] Where
 instance Show Select where
     show (Select fields from joins whr ) = 
-        let showList a b = show a ++ show b
-        in "SELECT " ++
-            foldl showList fields ++
-            show from ++
-            foldl showList joins ++
-            show whr
+        let 
+        in  "SELECT " ++
+            (show fields) ++
+            (show from) ++
+            (L.concat $ fmap show joins) ++
+            (show whr)
 
+
+
+
+
+
+        
+    
+
+
+
+--data InsertInto = InsertInto TableName [FieldName] Select
+--data Update     = Update TableName [FieldName] 
 --data Create     = 
 --data Drop       = 
