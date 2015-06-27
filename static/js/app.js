c1adae81edcbd87bc7f972aa4a444a9a056241fb
zezic <zezic51@yandex.ru>
2015-06-27 14:50:11 +0400

    Простые действия без перезагрузки страницы

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,43 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+    $('#upload').on('click', function(){
+        if (typeof $('#fileToUpload').prop('files')[0] !== 'undefined') {
+            var file_data = $('#fileToUpload').prop('files')[0];
+            var form_data = new FormData();
+            form_data.append('file', file_data);
+            $.ajax({
+                    url: 'upload',
+                    dataType: 'text',
+                    cache: false,
+                    contentType: false,
+                    processData: false,
+                    data: form_data,
+                    type: 'post',
+                    success: function(response){
+                        $('.status').text('DONE!');
+                    }
+            });
+            $('.status').text('Parsing file... Please wait.');
+        } else {
+            $('.status').text('No file selected.');
+        }
+    });
+
+    $('#ask').on('click', function(){
+        var tables = '["'+$('#requested-table').val()+'"]';
+        $.ajax({
+                url: 'addtables',
+                dataType: 'text',
+                cache: false,
+                contentType: false,
+                processData: false,
+                data: tables,
+                type: 'post',
+                success: function(response){
+                    $.get( "canvas", function( data ) {
+                        $( ".output" ).text( data );
+                        alert( "Load was performed." );
+                    });
+                }
+        });
+    });
 });
